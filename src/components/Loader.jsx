f8d9fddaacf230be3e667dfc8084d4586d2d85f9
apenduks1 <k77154313@gmail.com>
2025-07-21 18:41:08 +0300

    fix: replace react-loader-spinner with css loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,9 @@
-import { ThreeDots } from 'react-loader-spinner';
+import './Loader.css';
 
 export default function Loader() {
   return (
-    <div style={{ textAlign: 'center', padding: 20 }}>
-      <ThreeDots height="80" width="80" color="#3f51b5" />
+    <div className="loader-overlay">
+      <div className="loader" />
     </div>
   );
 }
